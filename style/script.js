3e544579caac39b5149a75543e8c5fb87ab6a20d
Selman <selmaneliyev99@gmail.com>
2023-11-19 16:50:50 +0400

    commit

---
+++ style/script.js
@@ -0,0 +1,153 @@
+// Asfalt hissesi
+let asfalt_span_ac = document.getElementById('asfalt-ac');
+let asfalt_span_bagla = document.getElementById('asfalt-bagla');
+let asfalt_metn = document.getElementById('asfalt-metn');
+
+asfalt_span_ac.addEventListener('click', () => {
+    asfalt_metn.style.display = 'block';
+
+    asfalt_span_ac.style.display = 'none';
+})
+
+asfalt_span_bagla.addEventListener('click', () => {
+    asfalt_metn.style.display = 'none';
+
+    asfalt_span_ac.style.display = 'inline-block';
+})
+
+
+// Dagidici hissesi
+let dagidici_span_ac = document.getElementById('dagidici-ac');
+let dagidici_span_bagla = document.getElementById('dagidici-bagla');
+let dagidici_metn = document.getElementById('dagidici-metn');
+
+dagidici_span_ac.addEventListener('click', () => {
+    dagidici_metn.style.display = 'block';
+
+    dagidici_span_ac.style.display = 'none';
+})
+
+dagidici_span_bagla.addEventListener('click', () => {
+    dagidici_metn.style.display = 'none';
+
+    dagidici_span_ac.style.display = 'inline-block';
+})
+
+// NTD hissesi
+let ntd_span_ac = document.getElementById('ntd-ac');
+let ntd_span_bagla = document.getElementById('ntd-bagla');
+let ntd_metn = document.getElementById('ntd-metn');
+
+ntd_span_ac.addEventListener('click', () => {
+    ntd_metn.style.display = 'block';
+
+    ntd_span_ac.style.display = 'none';
+})
+
+ntd_span_bagla.addEventListener('click', () => {
+    ntd_metn.style.display = 'none';
+
+    ntd_span_ac.style.display = 'inline-block';
+})
+
+// Qazma hissesi
+let qazma_span_ac = document.getElementById('qazma-ac');
+let qazma_span_bagla = document.getElementById('qazma-bagla');
+let qazma_metn = document.getElementById('qazma-metn');
+
+qazma_span_ac.addEventListener('click', () => {
+    qazma_metn.style.display = 'block';
+
+    qazma_span_ac.style.display = 'none';
+})
+
+qazma_span_bagla.addEventListener('click', () => {
+    qazma_metn.style.display = 'none';
+
+    qazma_span_ac.style.display = 'inline-block';
+})
+
+// Qurgular hissesi
+let qurgu_span_ac = document.getElementById('qurgu-ac');
+let qurgu_span_bagla = document.getElementById('qurgu-bagla');
+let qurgu_metn = document.getElementById('qurgu-metn');
+
+qurgu_span_ac.addEventListener('click', () => {
+    qurgu_metn.style.display = 'block';
+
+    qurgu_span_ac.style.display = 'none';
+})
+
+qurgu_span_bagla.addEventListener('click', () => {
+    qurgu_metn.style.display = 'none';
+
+    qurgu_span_ac.style.display = 'inline-block';
+})
+
+//Gemi
+let gemi_span_ac = document.getElementById('gemi-ac');
+let gemi_span_bagla = document.getElementById('gemi-bagla');
+let gemi_metn = document.getElementById('gemi-metn');
+
+gemi_span_ac.addEventListener('click', () => {
+    gemi_metn.style.display = 'block';
+
+    gemi_span_ac.style.display = 'none';
+})
+
+gemi_span_bagla.addEventListener('click', () => {
+    gemi_metn.style.display = 'none';
+
+    gemi_span_ac.style.display = 'inline-block';
+})
+
+//Reservuar
+let reservuar_span_ac = document.getElementById('reservuar-ac');
+let reservuar_span_bagla = document.getElementById('reservuar-bagla');
+let reservuar_metn = document.getElementById('reservuar-metn');
+
+reservuar_span_ac.addEventListener('click', () => {
+    reservuar_metn.style.display = 'block';
+
+    reservuar_span_ac.style.display = 'none';
+})
+
+reservuar_span_bagla.addEventListener('click', () => {
+    reservuar_metn.style.display = 'none';
+
+    reservuar_span_ac.style.display = 'inline-block';
+})
+
+//Rope
+let rope_span_ac = document.getElementById('rope-ac');
+let rope_span_bagla = document.getElementById('rope-bagla');
+let rope_metn = document.getElementById('rope-metn');
+
+rope_span_ac.addEventListener('click', () => {
+    rope_metn.style.display = 'block';
+
+    rope_span_ac.style.display = 'none';
+})
+
+rope_span_bagla.addEventListener('click', () => {
+    rope_metn.style.display = 'none';
+
+    rope_span_ac.style.display = 'inline-block';
+})
+
+//Konteyner
+let konteyner_span_ac = document.getElementById('konteyner-ac');
+let konteyner_span_bagla = document.getElementById('konteyner-bagla');
+let konteyner_metn = document.getElementById('konteyner-metn');
+
+konteyner_span_ac.addEventListener('click', () => {
+    konteyner_metn.style.display = 'block';
+
+    konteyner_span_ac.style.display = 'none';
+})
+
+konteyner_span_bagla.addEventListener('click', () => {
+    konteyner_metn.style.display = 'none';
+
+    konteyner_span_ac.style.display = 'inline-block';
+})
